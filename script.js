09087c41e0c55db1c02a8e28bd4b0e967dcceb24
Shubham277353 <golubhandari405@gmail.com>
2026-04-03 21:16:47 +0530

    feat: add js logic for multiple project
    created a array which will be iterated to load
    projects in DOM

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
 const projectsArray = [
     {
-        name : "Do-to-Doo"
-    }
+        name : "Do-to-Doo",
+        description: "This is a to-do-app , to manage tasks in the form of projects.",
+        imageUrl:"https://github.com/Shubham277353/Do-to-Dooo/raw/main/Images/Screenshot.png"
+    },
+    {
+        name : "Do-to-Doo",
+        description: "This is a to-do-app , to manage tasks in the form of projects.",
+        imageUrl:"https://github.com/Shubham277353/Do-to-Dooo/raw/main/Images/Screenshot.png"
+    },
+    {
+        name : "Do-to-Doo",
+        description: "This is a to-do-app , to manage tasks in the form of projects.",
+        imageUrl:"https://github.com/Shubham277353/Do-to-Dooo/raw/main/Images/Screenshot.png"
+    },
+    {
+        name : "Do-to-Doo",
+        description: "This is a to-do-app , to manage tasks in the form of projects.",
+        imageUrl:"https://github.com/Shubham277353/Do-to-Dooo/raw/main/Images/Screenshot.png"
+    },
+    {
+        name : "Do-to-Doo",
+        description: "This is a to-do-app , to manage tasks in the form of projects.",
+        imageUrl:"https://github.com/Shubham277353/Do-to-Dooo/raw/main/Images/Screenshot.png"
+    },
+    {
+        name : "Do-to-Doo",
+        description: "This is a to-do-app , to manage tasks in the form of projects.",
+        imageUrl:"https://github.com/Shubham277353/Do-to-Dooo/raw/main/Images/Screenshot.png"
+    },
 ]
 
+const container = document.getElementById("projects-conatiner");
+
+projectsArray.forEach((project)=>{
+    const projectCard = document.createElement("div")
+    projectCard.classList.add("project-card");
+
+    const imageContainer = document.createElement("img");
+    imageContainer.src = project.imageUrl;
+
+    const Heading = document.createElement("h3");
+    Heading.textContent = project.name;
+
+    const description = document.createElement("p");
+    description.textContent = project.description;
+
+    projectCard.append(imageContainer,Heading,description);
+    container.append(projectCard);
+
+
+})
 
 
 
